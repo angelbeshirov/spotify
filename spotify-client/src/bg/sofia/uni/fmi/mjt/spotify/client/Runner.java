c8aa5001bea1174ac89fe5eac6c0c34700afda15
angelbeshirov <angel.beshirov@abv.bg>
2020-01-31 03:25:00 +0200

    initial commit

---
+++ spotify-client/src/bg/sofia/uni/fmi/mjt/spotify/client/Runner.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.spotify.client;
+
+/**
+ * @author angel.beshirov
+ */
+public class Runner {
+}
---
+++ spotify-server/src/bg/sofia/uni/fmi/mjt/spotify/server/Server.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.spotify.server;
+
+/**
+ * @author angel.beshirov
+ */
+public class Server {
+}
